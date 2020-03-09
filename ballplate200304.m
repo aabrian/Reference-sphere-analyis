c4b0a6ea16326e0e325ed001e3759a6152916f70
Amaya <baa4@NIST.GOV>
2020-03-09 15:47:48 -0400

    updated

--- ballplate200304.m
+++ ballplate200304.m
@@ -6,7 +6,7 @@ folder1 = fullfile(folderMain,folders.name);
 files = dir(fullfile(folder1,'*.asc'));
 %Read the file
 flag = 0;
-for i = 1:length(files)
+for i = 1:length(files) %for each file cluster data into N clusters using rotation matrix
     fname1 = fullfile(folder1,files(i).name);
     data1 = dlmread(fname1);
     
@@ -42,41 +42,72 @@ for i = 1:length(files)
             text(cent(1),cent(2),cent(3)+20,num2str(ii));
             pause(.1);
         end
-            %approximate centers for corner and center spheres
-            centerscol = [-106.3 -30.69 332.5; 140.6 9.799 330.7; 17.4 -10.83 205.1; ...
-                -106.5 -31.33 78.8; 141.8 8.783 78.89];
-            centersrow = centerscol';
-        NROI = 5; %number of ROIs
-        %finding ROI
-        CenterROI = zeros(length(NROI),3);
-        for iii = 1:5 %finding center and Radius of each ROI
-            ROIdata = getROI(dataA,centerscol(iii),20);
-            
-            [sphereROI] = spherefit2(ROIdata(:,1),ROIdata(:,2),ROIdata(:,3));
-            CenterROI(iii,1) = sphereROI.Center(1);  CenterROI(iii,2) = sphereROI.Center(2);  CenterROI(iii,3) = sphereROI.Center(3);
-            diameterROI(iii) = sphereROI.Radius*2;
-        end
-        j = 1;
-        h = 1;
-        count = 1;
-        for jj = 1:NROI-1
-            j = jj;
-            while j < NROI
-                dist1(count) = rssq(CenterROI(jj)-CenterROI(h+1))
-                j = j+1;
-                h = j+1;
-                count = count + 1;
-            end
-            h = jj+1;
+        %approximate centers for corner and center spheres
+        centerscol = [-106.3 -30.69 332.5; 140.6 9.799 330.7; 17.4 -10.83 205.1; ...
+            -106.5 -31.33 78.8; 141.8 8.783 78.89];
+        centersrow = centerscol';
+        
+        
+    end
+    NROI = 5; %number of ROIs
+    %finding ROI
+    for iii = 1:NROI %finding center and Radius of each ROI
+        ROIdata = getROI(data1,centerscol(iii,:),20);
+        
+        sphereROI = spherefit2(ROIdata(:,1),ROIdata(:,2),ROIdata(:,3));
+        centerROI(iii,1) = sphereROI.Center(1);  centerROI(iii,2) = sphereROI.Center(2);  centerROI(iii,3) = sphereROI.Center(3);
+        diameterROI(iii) = sphereROI.Radius*2;
+        
+        
+    end
+    count = 1;
+    for jj = 1:NROI
+        j = jj;
+        h = jj;
+        while j < NROI
+            dist(count) = rssq(centerROI(jj,:) - centerROI(h+1,:));
+            j = j+1;
+            h = j;
+            count = count + 1;
         end
     end
-    %     disp(files(i).name)
-    %     pause
+    scatter3(data1(:,1),data1(:,2),data1(:,3),.7);
+    hold on
+    for q = 1: NROI
+        scatter3(centerROI(q,1),centerROI(q,2),centerROI(q,3),3,'filled');
+    end
+    hold off
+    dist1(i) = dist(1);
+    dist2(i) = dist(2);
+    dist3(i) = dist(3);
+    dist4(i) = dist(4);
+    dist5(i) = dist(5);
+    dist6(i) = dist(6);
+    dist7(i) = dist(7);
+    dist8(i) = dist(8);
+    dist9(i) = dist(9);
+    dist10(i) = dist(10);
     
-    
-    
-    
-    
-    
-    
+    angleint = [8 16 12 32 40 48 46 64 72 80 -56 -48 -40 -32 -24 -16 -8 0];
 end
+
+figure(1)
+scatter(angleint,dist1)
+figure(2)
+scatter(angleint,dist2)
+figure(3)
+scatter(angleint,dist3)
+figure(4)
+scatter(angleint,dist4)
+figure(5)
+scatter(angleint,dist5)
+figure(6)
+scatter(angleint,dist6)
+figure(7)
+scatter(angleint,dist7)
+figure(8)
+scatter(angleint,dist8)
+figure(9)
+scatter(angleint,dist9)
+figure(10)
+scatter(angleint,dist10)
